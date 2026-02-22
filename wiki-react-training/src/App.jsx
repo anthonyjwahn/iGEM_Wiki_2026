c56099912f83cc02dfbf19e5e68c63deedeee3ab
tywendywu <Wendywu7425@gmail.com>
2026-02-22 16:42:57 -0500

    updated

--- wiki-react-training/src/App.jsx
+++ wiki-react-training/src/App.jsx
@@ -1,8 +1,16 @@
+import './App.css';
+
 function App() {
+  const teamName = "Cornell iGEM";
+  const memberCount = 49;
+  const foundedYear = 2009;
+  const yearsActive = 2026 - foundedYear;
+  
   return (
     <div>
-      <h1>Hi wiki.</h1>
-      <p>Welcome to React!</p>
+      <h1>Welcome to {teamName}!</h1>
+      <p>We have {memberCount} amazing members.</p>
+      <p>Founded in {foundedYear} ({yearsActive} years ago!)</p>
     </div>
   );
 }
