152b06779faa8b5b797766bf9ac87bacee76ee8e
Alex Peng <ap2462@cornell.edu>
2026-02-22 13:30:55 -0500

    Coding

--- Project_3/exercises.js
+++ Project_3/exercises.js
@@ -102,4 +102,177 @@ let self_major = self.major;
 console.log(`Name: ${self_name}, Major: ${self_major}`);
 
 //3.4
-let objarray = [{name: "Anthony", role: "Player", year: "1999"}, ]
+let objarray = [{name: "Anthony", role: "Team Lead", year: "1999"}, {name: "Eujin", role: "Rejected", year: "1972"}, {name: "George", role: "Spectator", year: "2005"}]
+
+//3.5
+newArray = objarray.map(object => object.name);
+roleArray = objarray.filter(object => object.role === "Rejected");
+console.log(newArray);
+console.log(roleArray);
+
+//Part 4: Functions & Logic
+
+//4.1 
+function gcalc (grade) {
+    if (grade >= 90) {
+        return "A";
+    } else if (grade >= 80 && grade < 90) {
+        return "B";
+    } else if (grade >= 70 && grade < 80) {
+        return "C";
+    } else if (grade >= 60 && grade < 70) {
+        return "D";
+    }
+}
+console.log(gcalc(79));
+
+//4.2
+function findLongest (array) {
+    var length = 0;
+    var longest;
+    for (i = 0; i < array.length; i++) {
+        if (array[i].length > length) {
+            var length = array[i].length;
+            var longest = array[i];
+        }
+    }
+    return longest;
+}
+let stringArray = ["cat", "elephant", "dog"];
+console.log(findLongest(stringArray));
+
+
+//4.3
+function count (string, letter) {
+    let char = letter;
+    let count = 0;
+    for (j = 0; j < string.length; j++) {
+        if (char === string.charAt(j)) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(count("hello", "l"));
+console.log(count("banana", "a"));  
+
+//4.4 
+function reverseArray (array, newArray) {
+    let count = 0;
+    for (let i = array.length - 1; i >= 0; i--) {
+        newArray[count] = array[i];
+        count++;
+    }
+}
+let secondArray = [];
+reverseArray([1, 2, 3], secondArray);
+console.log(secondArray);
+
+//4.5
+function fizzBuzz (number) {
+    let output = "";
+    for (i = 1; i <= number; i++) {
+        if (i % 3 === 0) {
+            output += "Fizz ";
+        } else if (i % 5 === 0) {
+            output += "Buzz ";
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            output += "FizzBuzz";
+        } else {
+            output += i + " ";
+        }
+    }
+    console.log(output.trim());
+}
+fizzBuzz(19);
+
+//Part 5: Putting it together
+
+//5.1
+
+const team = [
+  { name: "Anthony", role: "Lead", yearsExp: 2 },
+  { name: "Alex", role: "Developer", yearsExp: 2 },
+  { name: "Audrey", role: "Designer", yearsExp: 2 },
+  { name: "Eujin", role: "Advisor", yearsExp: 3 },
+  { name: "Max", role: "Developer", yearsExp: 1 },
+  { name: "Nicole", role: "Designer", yearsExp: 2 },
+  { name: "Wendy", role: "Designer", yearsExp: 1 },
+  { name: "Winnie", role: "Developer", yearsExp: 2 }
+];
+
+function nameArray (array) {
+    newArray = array.map(object => object.name);
+    console.log(newArray);
+}
+
+function roleSearch (array) {
+    newArray = array.filter(object => object.role === "Developer");
+    console.log(newArray);
+}
+
+function avgXp (array) {
+    newArray = array.map(object => object.yearsExp);
+    let sum = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += newArray[i];
+    }
+    let avg = sum/array.length;
+    console.log(avg);
+}
+
+function Xp (array) {
+    let a = array[0];
+    for (i = 1; i < array.length; i++) {
+        if (array[i].yearsExp > a.yearsExp) {
+            a = array[i];
+        }
+    }
+    console.log(a);
+}
+
+isExperienced = team.map(object => {
+    return {
+        ...object,
+        isExperienced: object.yearsExp >= 4
+    };
+});
+
+nameArray(team);
+roleSearch(team);
+avgXp(team);
+Xp(team);
+console.log(isExperienced);
+
+//5.2
+const cart = [
+  { item: "Laptop", price: 999, quantity: 1 },
+  { item: "Mouse", price: 25, quantity: 2 },
+  { item: "Keyboard", price: 75, quantity: 1 },
+  { item: "Monitor", price: 300, quantity: 2 }
+];
+
+function getTotal (array) {
+    sum = array.reduce((total, item) => {
+        return total + (item.price * item.quantity)
+    }, 0);
+    console.log(sum);
+}
+
+function getItemNames (array) {
+    newArray = array.map(object => object.item);
+    console.log(newArray);
+}
+
+function findExpensive (array, lb) {
+    newArray = array.filter(item => item.price > lb);
+    console.log(newArray);
+}
+
+function addCart (array, newItem) {
+    return [...cart, newItem];
+}
+getTotal(cart);
+getItemNames(cart);
+findExpensive(cart, 100);
+console.log(addCart(cart, {item: "Stand", price: 40, quantity: 1}));
